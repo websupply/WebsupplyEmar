dd4ff621a062d0e386c5bd3e836b9854898fe1a4
Luis Gustavo Hunzecher de Castro <luzbolinha@hotmail.com>
2024-09-13 14:42:08 -0300

    Deixando os parametros do DataTable como Null

--- WebsupplyEmar.Dominio/Model/DataTableModel.cs
+++ WebsupplyEmar.Dominio/Model/DataTableModel.cs
@@ -8,31 +8,31 @@ namespace WebsupplyEmar.Dominio.Model
 {
     public class DataTableModel
     {
-        public List<ColumnDto> Columns { get; set; }
-        public List<OrderDto> Order { get; set; }
-        public SearchDto Search { get; set; }
-        public int Start { get; set; }
-        public int Length { get; set; }
+        public List<ColumnDto>? Columns { get; set; }
+        public List<OrderDto>? Order { get; set; }
+        public SearchDto? Search { get; set; }
+        public int? Start { get; set; }
+        public int? Length { get; set; }
 
         public class ColumnDto
         {
-            public string Data { get; set; }
-            public string Name { get; set; }
-            public bool Searchable { get; set; }
-            public bool Orderable { get; set; }
-            public SearchDto Search { get; set; }
+            public string? Data { get; set; }
+            public string? Name { get; set; }
+            public bool? Searchable { get; set; }
+            public bool? Orderable { get; set; }
+            public SearchDto? Search { get; set; }
         }
 
         public class OrderDto
         {
-            public int Column { get; set; }
-            public string Dir { get; set; }
+            public int? Column { get; set; }
+            public string? Dir { get; set; }
         }
 
         public class SearchDto
         {
-            public string Value { get; set; }
-            public bool Regex { get; set; }
+            public string? Value { get; set; }
+            public bool? Regex { get; set; }
         }
     }
 }
