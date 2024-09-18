6c8ba4d06d406e5b638ab6f98f4b5a40662b2e6f
Luis Gustavo Hunzecher de Castro <luzbolinha@hotmail.com>
2024-09-18 14:50:54 -0300

    removendo a variavel ambiente que ficou no arquivo constantes.js

--- WebsupplyEmar.API/wwwroot/assets/js/constantes.js
+++ WebsupplyEmar.API/wwwroot/assets/js/constantes.js
@@ -3,10 +3,10 @@ const baseUrl = `${window.location.protocol}//${window.location.host}/WSEmaR`;
 
 // Metodos da API
 const apiUrl = {
-    logs_emar: `${baseUrl}${ambiente}/api/Cockpit/logs_emar`,
-    logs_emar_processamentos: `${baseUrl}${ambiente}/api/Cockpit/logs_emar_processamentos`,
-    logs_websocket: `${baseUrl}${ambiente}/api/Cockpit/logs_websocket`,
-    cards: `${baseUrl}${ambiente}/api/Cockpit/cards`,
-    dados_servidor_websocket: `${baseUrl}${ambiente}/api/Cockpit/dados_servidor_websocket`,
-    dados_ambiente: `${baseUrl}${ambiente}/api/Cockpit/dados_ambiente`
+    logs_emar: `${baseUrl}/api/Cockpit/logs_emar`,
+    logs_emar_processamentos: `${baseUrl}/api/Cockpit/logs_emar_processamentos`,
+    logs_websocket: `${baseUrl}/api/Cockpit/logs_websocket`,
+    cards: `${baseUrl}/api/Cockpit/cards`,
+    dados_servidor_websocket: `${baseUrl}/api/Cockpit/dados_servidor_websocket`,
+    dados_ambiente: `${baseUrl}/api/Cockpit/dados_ambiente`
 }
