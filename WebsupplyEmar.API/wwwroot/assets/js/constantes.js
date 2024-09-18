9642ac799d349217defa4bc2084c7ec44cf81808
Luis Gustavo Hunzecher de Castro <luzbolinha@hotmail.com>
2024-09-18 13:41:45 -0300

    Ajuste caso o Ambiente seja localhost

--- WebsupplyEmar.API/wwwroot/assets/js/constantes.js
+++ WebsupplyEmar.API/wwwroot/assets/js/constantes.js
@@ -1,12 +1,12 @@
 // Url do Ambiente
-const baseUrl = `${window.location.protocol}//${window.location.host}/WSEmaR`;
+const baseUrl = `${window.location.protocol}//${window.location.host}`;
 
 // Metodos da API
 const apiUrl = {
-    logs_emar: `${baseUrl}/api/Cockpit/logs_emar`,
-    logs_emar_processamentos: `${baseUrl}/api/Cockpit/logs_emar_processamentos`,
-    logs_websocket: `${baseUrl}/api/Cockpit/logs_websocket`,
-    cards: `${baseUrl}/api/Cockpit/cards`,
-    dados_servidor_websocket: `${baseUrl}/api/Cockpit/dados_servidor_websocket`,
-    dados_ambiente: `${baseUrl}/api/Cockpit/dados_ambiente`
+    logs_emar: `${baseUrl}${ambiente}/api/Cockpit/logs_emar`,
+    logs_emar_processamentos: `${baseUrl}${ambiente}/api/Cockpit/logs_emar_processamentos`,
+    logs_websocket: `${baseUrl}${ambiente}/api/Cockpit/logs_websocket`,
+    cards: `${baseUrl}${ambiente}/api/Cockpit/cards`,
+    dados_servidor_websocket: `${baseUrl}${ambiente}/api/Cockpit/dados_servidor_websocket`,
+    dados_ambiente: `${baseUrl}${ambiente}/api/Cockpit/dados_ambiente`
 }
